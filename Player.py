b757889153b84c731f954e15b96f37504f248860
GAIO DOS SANTOS Lucas <lucas.gaildossantos@gmail.com>
2021-05-31 23:48:19 +0200

    remove player content and add name attribut

--- Player.py
+++ Player.py
@@ -1,34 +1,4 @@
 class Player:
-    def __int__(self):
-        self.cordinates = (0,0)
-        self.name = "player"
-        self.health = 100
-        self.max_health = 100
-        self.attack = 10
-        self.defence = 10
-        self.luck = 10
-        self.velocity = 10
+    def __int__(self, name):
+        self.name = name
 
-    def move_right(self):
-        self.rect.x += self.velocity
-
-    def move_left(self):
-        self.rect.x -= self.velocity
-#verifier la santé ou l'attaque du personnage
-
-    def getHealth(self):
-        return self.health
-    def getAttack(self):
-        return self.attack
-    def getLuck(self):
-        return self.luck
-    def getDefence(self):
-        return self.defence
-    def getName(self):
-        return self.name
-
-        #player = player()
-        #self.pressed = {
-        #    "touche fleche_droite":True,
-        #    "touche fleche_gauche": False
-        #}
