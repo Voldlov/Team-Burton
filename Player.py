3e0ab583c242be7561b0f574e2bb1c2eb53748da
GAIO DOS SANTOS Lucas <lucas.gaildossantos@gmail.com>
2021-06-01 12:54:59 +0200

    add all code for sql in player, no request

--- Player.py
+++ Player.py
@@ -7,17 +7,33 @@ class Player:
         #cn = connector.connect(user='', password='', host='localhost', database='')
         self.name = name
         result = self.verify(name)
-        if result :
-            self.recovery()
+        if result:
+            if self.passWord() == 1:
+                self.recovery()
         else :
             self.creat()
 
     def creat(self, name):
         #création d'une nouvelle ligne dans la table
+        #la requête
+        q = ''
+        #créer un curseur
+        c = cn.cursor()
+        #envoyer la requête
+        c.execute(q)
 
     def verify(self, name):
         #le nom est-il déjà utilisé ?
-        return 0
+        #la requête
+        q = 'SELECT *** FROM ***'
+        #créer un curseur
+        c = cn.cursor()
+        #envoyer la requête et recevoir le résultat
+        if name == c.execute(q) :
+            a = 1
+        else :
+            a = 0
+        return a
 
     def recovery(self):
         #récupération des information dans la table
@@ -43,6 +59,12 @@ class Player:
 
     def save(self):
         #sauvegarde des informations
+        #la requête
+        q = ''
+        #créer un curseur
+        c = cn.cursor()
+        #envoyer la requête
+        c.execute(q)
 
     def close(self):
         #fermer la connexion avec la base de donnée
