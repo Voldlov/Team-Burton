a70f8ac7ab26c8d00e18d6e511a83bc45fd46088
Chris <nico.bar2012@gmail.com>
2021-06-01 17:06:14 +0200

    rename interface to distr

---
+++ distr/main.py
@@ -0,0 +1,9 @@
+#ouverture
+class main:
+    def __init__(self):
+        if info :
+            #ouvrir le jeu
+            #changement de page QT et lancement de Game
+        else :
+            #fermer
+            #commande QT pour fermer la fenêtre
