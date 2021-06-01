6a7a7fbf6ff95a3cae812ab83146e4da2dda477a
GAIO DOS SANTOS Lucas <lucas.gaildossantos@gmail.com>
2021-06-01 17:42:29 +0200

    add interface +

--- interfaceCeolcyn/main.py
+++ interfaceCeolcyn/main.py
@@ -3,17 +3,26 @@ from tkinter import *
 #Création de la fenêtre
 fenetre = Tk()
 
+#couleur de fond
+fenetre['bg']='grey'
+
+#titre de la fenêtre
+fenetre.title("ceolcyn")
+#taille de la fenêtre
+fenetre.geometry("1000x900")
+#icone
+fenetre.iconbitmap("Ceolcyn_Dragon_1.ico")
+
 #texte d'intro
-label = Label(fenetre, text="Ceolcyn")
-label.pack()
+label = Label(fenetre, text="Ceolcyn").pack()
+
+cadre = Frame(fenetre).pack(side=BOTTOM,padx=10,pady=10)
 
 #bouton pour fermer le jeu
-bouton=Button(fenetre, text="sortir", command=fenetre.quit)
-bouton.pack()
+bouton=Button(cadre, text="sortir", relief=RIDGE, command=fenetre.quit,cursor="pirate").pack(side=RIGHT)
 
 #bouton pour rentrer dans le jeu
-bouton=Button(fenetre, text="entrer")
-bouton.pack()
+bouton=Button(cadre, text="entrer", relief=RIDGE).pack(side=LEFT)
 
 #ouverture de la fenêtre
 fenetre.mainloop()
